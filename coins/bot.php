021e2c4770476af43483c90689d76b9aa02ff656
vnesnasnow <nemalvinay@gmail.com>
2024-10-10 16:09:39 +0300

    Update bot.php

--- coins/bot.php
+++ coins/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7848139127:AAER6pf7vMIpDdPfGsFFWtbmyL7Sfj_SC5g";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
